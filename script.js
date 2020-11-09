8dee91f74869f34f034f2d7dcc9a5c0e251d106a
Debora <deboraro.61@gmail.com>
2020-11-09 16:24:04 -0300

    gerado o background do jogo

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let canvas = document.querySelector(".canvas");
-let contexto = document.getContext("2d");
+let canvas = document.querySelector("#canvas");
+let contexto = canvas.getContext("2d");
 
 let box = 32;
 
 function criaBG(){
-  contexto.fillStyle = "blue";
+  contexto.fillStyle = "lightgreen";
   contexto.fillRect(0, 0, 16*box, 16*box);
 }
 
 criaBG();
 
-alert("ola");
