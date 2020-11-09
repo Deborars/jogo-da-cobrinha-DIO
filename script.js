d0290e08811e059537e44ffd81b74236c78cd2e9
Debora <deboraro.61@gmail.com>
2020-11-09 16:36:48 -0300

    gerada a função que cria a cobrinha e aula 1 concluída

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let canvas = document.querySelector("#canvas");
 let contexto = canvas.getContext("2d");
-
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+};
 
 function criaBG(){
   contexto.fillStyle = "lightgreen";
   contexto.fillRect(0, 0, 16*box, 16*box);
 }
+
+function criarCobrinha(){
+  for(let i = 0; i < snake.length; i++){
+    contexto.fillStyle = "green";
+    contexto.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+
+
 
 criaBG();
 
